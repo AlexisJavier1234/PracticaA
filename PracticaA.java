458ee66f9cf97c8badee8720dc956e6578187821
AlexisJavier1234 <143725720+AlexisJavier1234@users.noreply.github.com>
2023-09-14 09:28:52 -0500

    Add files via upload

---
+++ PracticaA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicaa;
+import java.util.Scanner;
+/**
+ *
+ * @author labcca
+ */
+public class PracticaA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     double dolares= 17;
+     double euro =18.29;
+     Scanner lecturaDatos = new Scanner(System.in);
+     System.out.println("Ingrese cantidad en peso ");
+      double pesos = lecturaDatos.nextInt();
+      double totalDolares = pesos/dolares;
+     System.out.println("La cantida en dolares es: "+totalDolares);
+     double totalEuros = pesos/euro;
+     System.out.println("La cantida en euro es: "+totalEuros);
+    }
+}
